aa476dc5f3acac1f0539c352bfd2607b14be749f
bjornua <bjorn.arnholtz@gmail.com>
2015-05-01 09:06:59 +0200

    Make it work!

--- js/main.js
+++ js/main.js
@@ -1,43 +1,49 @@
 define(["react", "immutable", "vector"], function (React, Immutable, Vector) {
     "use strict";
 
-    var Rectangle = Immutable.Record({
-        topLeft: undefined,
-        s: undefined
-    }, "Rectangle");
-
     var toPercent = function (val) {
         return (val * 100).toFixed(4) + "%";
     };
-
 
     var Tile = React.createClass({
         displayName: "Tile",
         render: function () {
             var self = this;
+            var pos = self.props.pos;
+            var size = new Vector(1, 1);
+            var dir = this.props.orientation;
 
-            vx = Immutable.List([self.props.pos, new Vector([1, 1])]);
+            var scale = function (vec) {
+                return vec.divide(self.props.grid).multiply(self.props.canvas);
+            };
 
-            rect = rect.map(function (v) {
-                var divided = v.divide(self.props.grid);
-                var percentage = divided.map(toPercent);
-                return percentage;
-            });
+            pos = scale(pos);
+            size = scale(size);
 
-
+            if (dir === 1 || dir === 3) {
+                pos = pos.add(size.subtract(size.invert()).divide(new Vector(2, 2)));
+                size = size.invert();
+            }
+            var rot = dir * 90;
             var style = Immutable.fromJS({
                 position: "absolute",
+                left: pos.x.toFixed(2) + "px",
+                top: pos.y.toFixed(2) + "px",
+                width: (size.x + 1).toFixed(2) + "px",
+                height: (size.y + 1).toFixed(2) + "px",
+                lineHeight: size.y.toFixed(2) + "px",
+                transform: "rotate(" + rot + "deg)",
+                fontSize: size.y.toFixed(2) + "px",
                 overflow: "hidden",
-                left: rect.topLeft.x,
-                top: rect.topLeft.y,
-                width: rect.s.x,
-                bottom: rect.s.y,
+                verticalAlign: "middle",
+                boxSizing: "border-box",
                 border: "1px solid rgba(0, 50, 0, 1)"
             });
             style = style.merge(style, self.props.style);
 
-            return React.createElement("div", {style: style.toJS()}, self.props.children);
-
+            return React.createElement("div", {
+                id: "tile_" + this.props.pos.x + "_" + this.props.pos.y,
+                style: style.toJS()}, self.props.children);
         }
     });
     var GrassTile = React.createClass({
@@ -45,12 +51,13 @@ define(["react", "immutable", "vector"], function (React, Immutable, Vector) {
             return React.createElement(Tile, {
                 pos: this.props.pos,
                 grid: this.props.grid,
+                canvas: this.props.canvas,
                 style: {
                     // background: "url('./img/GrassGreenTexture0001.jpg')",
                     background: "url('./img/grass.jpg')",
                     "backgroundSize": "100% 100%"
-
-                }
+                },
+                orientation: 0
             });
         }
     });
@@ -62,13 +69,14 @@ define(["react", "immutable", "vector"], function (React, Immutable, Vector) {
             return React.createElement(Tile, {
                 pos: this.props.pos,
                 grid: this.props.grid,
+                canvas: this.props.canvas,
                 style: {
                     background: "rgba(255, 0, 0, 0.2)",
                     border: "1px solid rgba(255, 0, 0, 0.4)",
-                    fontSize: "100px",
-                    lineHeight: "67px",
-                    transform: "rotate(90deg)"
-                }
+                    fontSize: "40px",
+                    textAlign: "right"
+                },
+                orientation: 3
             }, "→");
 
 
@@ -78,23 +86,32 @@ define(["react", "immutable", "vector"], function (React, Immutable, Vector) {
     var GridWidget = React.createClass({
         displayName: "Grid",
 
+
         render: function () {
             var self = this;
-            var tiles = Immutable.Range(0, self.props.grid.get(0)).map(function (y) {
-                return Immutable.Range(0, self.props.grid.get(1)).map(function (x) {
-                    var pos = new Vector([x, y]);
-                    return React.createElement(GrassTile, {pos: pos, grid: self.props.grid, key: String(x) + "_" + String(y)});
+            var canvas = new Vector(1000, 600);
+            var tiles = Immutable.Range(0, self.props.grid.y).map(function (y) {
+                return Immutable.Range(0, self.props.grid.x).map(function (x) {
+                    var pos = new Vector(x, y);
+                    return React.createElement(GrassTile, {
+                        pos: pos,
+                        grid: self.props.grid,
+                        key: String(x) + "_" + String(y),
+                        canvas: canvas
+                    });
                 });
             }).flatten(1);
 
+
             // React.createElement(Tile, {x: 0, y: 0, width: 4, height: 3}),
             return React.createElement("div", {style: {
-                width: "1000px",
-                height: "1000px",
-                position: "relative"
+                width: String(canvas.x) + "px",
+                height: String(canvas.y) + "px",
+                position: "relative",
+                overflow: "hidden"
             }},
                 tiles.toJS(),
-                React.createElement(LawnMower, {pos: new Vector([0, 0]), grid: self.props.grid})
+                React.createElement(LawnMower, {pos: new Vector(9, 1), grid: self.props.grid, canvas: canvas})
             );
 
         }
@@ -103,7 +120,7 @@ define(["react", "immutable", "vector"], function (React, Immutable, Vector) {
     var Main = React.createClass({
         displayName: "main",
         render: function () {
-            return React.createElement(GridWidget, {grid: new Vector([10, 10])});
+            return React.createElement(GridWidget, {grid: new Vector(30, 20)});
         }
     });
 
--- js/vector.js
+++ js/vector.js
@@ -1,25 +1,42 @@
-define(["immutable"], function (Immutable) {
+define([], function () {
     "use strict";
 
-    class Vector extends Immutable.List {
+    class Vector {
+        constructor(x, y) {
+            this.x = x;
+            this.y = y;
+            Object.freeze(this);
+        }
         add(other) {
-            return this.mergeWith(function(a, b) {
-                return a + b;
-            }, other);
-        };
+            return new Vector(
+                this.x + other.x,
+                this.y + other.y
+            );
+        }
         subtract(other) {
-            return this.mergeWith(function(a, b) {
-                return a - b;
-            }, other);
-        };
+            return new Vector(
+                this.x - other.x,
+                this.y - other.y
+            );
+        }
         divide(other) {
-            return this.mergeWith(function(a, b) {
-                return a / b;
-            }, other);
-        };
+            return new Vector(
+                this.x / other.x,
+                this.y / other.y
+            );
+        }
+        multiply(other) {
+            return new Vector(
+                this.x * other.x,
+                this.y * other.y
+            );
+        }
         invert() {
-            return Vector([this.get(1), this.get(0)]);
-        };
+            return new Vector(this.y, this.x);
+        }
+        map (f) {
+            return new Vector(f(this.x), f(this.y));
+        }
     }
 
     return Vector;
