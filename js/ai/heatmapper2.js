e5797f21fa5eee24aa3327984e9f4675d2ef34de
bjornua <bjorn.arnholtz@gmail.com>
2015-05-23 16:15:39 +0200

    fix

--- js/ai/heatmapper2.js
+++ js/ai/heatmapper2.js
@@ -15,10 +15,10 @@ define(["vector", "immutable", "ai/tools2"], function (Vector, Immutable, Tools)
 
     };
 
-    var mapTiles = function(game) {
-        game.area.tiles.forEach(function(initialTile) {
-            Tools.bfs(initialTile.pos, game.area).takeWhile(function(res) {
-                return res.dist < 4;
+    var mapTiles = function(centerPos, game) {
+        Tools.bfs(centerPos, game.area).forEach(function(startTile) {
+            Tools.bfs(startTile.pos, game.area).takeWhile(function(res) {
+                return true;
             }).forEach(function (res) {
                 game = updateScore(game, res.pos, res.dist);
             });
@@ -26,9 +26,16 @@ define(["vector", "immutable", "ai/tools2"], function (Vector, Immutable, Tools)
         return game;
     };
 
+    var getPath = function () {
+
+    };
+
     return function (game) {
         if (game.getState("initialized") !== true) {
-            game = mapTiles(game);
+            Tools.bfs(Vector(0, 0), game.area).forEach(function (res) {
+                game = game.setNumber(res.pos, undefined);
+            });
+            game = mapTiles(game.pos, game);
             game = game.setState("initialized", true);
         } else {
             game = game.moveForward();
